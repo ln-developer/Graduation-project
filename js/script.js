f7e5fe5ac43d25483fab1d2bb8d11d8cdc9a1435
ln-developer <liana.nch1502@gmail.com>
2020-09-04 03:01:57 +0300

    last version

--- js/script.js
+++ js/script.js
@@ -64,61 +64,150 @@ $(document).ready(function(){
 
     // MENU
 
-    const burger = document.querySelector('.menu-burger_header_mobile');
-    const close = document.querySelector('.main-menu__button_exit');
-    const menu = document.querySelector('.main-menu_header_mobile');
-    const hidden = document.querySelector('.html');
+    $('.menu-burger_header_mobile').on('click', function(event){
+        $('.main-menu_header_mobile').addClass('menu_showed');
+        $('html').addClass('llock');
+    });
 
-    function showMenu() {
-        menu.classList.add('menu_showed');
-        hidden.classList.add('html-hidden');
+    $('.main-menu__button_exit').on('click', function(){
+        $('.main-menu_header_mobile').removeClass('menu_showed');
+        $('html').removeClass('llock');
+    });
+
+    $('.menu__link_mobile').on('click', function(){
+        $('.main-menu_header_mobile').removeClass('menu_showed');
+        $('html').removeClass('llock');
+    });
+
+    // POPUP&&POPUP-GRADITUDE  
+
+    function popupClose() {
+      $('.popup__input').removeClass('error');
+      $('.popup').removeClass('popup_show');
+      $('.popup__input').val('');
     }
 
-    function closeMenu() {
-        menu.classList.remove('menu_showed');
-        hidden.classList.remove('html-hidden');
+    function popupGraditudeOpen() {
+      $('.popup-graditude').addClass('popup-graditude_show');
+      setTimeout(function(){
+        $('html').removeClass('llock');
+        $('.popup-graditude').removeClass('popup-graditude_show');
+      }, 2000);
     }
-
-    burger.addEventListener('click', showMenu);
-    close.addEventListener('click', closeMenu);
 
     // POPUP-CALL
 
-    const popupOpenCall = document.querySelectorAll('.popup_call_open');
-    const popupCloseCall = document.querySelector('.popup__button_exit_call');
-    const popupCall = document.querySelector('.popup_call');
+    $('.popup_call_open').on('click', function(){
+        $('.popup_call').addClass('popup_show');
+        $('html').addClass('llock');
+    });
 
-    function showPopupCall() {
-      popupCall.classList.add('popup_show');
-      hidden.classList.add('html_hidden');
+    $('.popup__button_exit_call').on('click', function(){
+        $('.popup_call').removeClass('popup_show');
+        $('.popup__input').removeClass('error');
+        $('.popup__input').val('');
+        $('html').removeClass('llock');
+    });
+
+    function popupOrderCall() {
+      let nameCall = $('#popup__input_name_call').val(),
+          telCall = $('#popup__input_tel_call').val();
+
+      if (nameCall.length !== 0 && telCall.length == 16){
+          popupClose();
+      }
+
+      else if (nameCall.length == 0 && telCall.length == 16){
+          $('#popup__input_name_call').addClass('error');
+          return false;
+      }
+  
+      else if (nameCall.length !== 0 && telCall.length !== 16){
+          $('#popup__input_tel_call').addClass('error').val('');
+          return false;
+      }
+  
+      else {
+          $('.popup__input').addClass('error');
+          setTimeout(function(){
+            $('.popup__input').removeClass('error');
+          }, 1500);
+          return false;
+      }
+
+      popupGraditudeOpen();
     }
 
-    function closePopupCall() {
-      popupCall.classList.remove('popup_show');
-      hidden.classList.remove('html_hidden');
+    $('.popup__input_send_call').on('click', popupOrderCall);
+
+    //POPUP-ORDER
+
+    $('.popup_order_open').on('click', function(){
+        $('.popup_order').addClass('popup_show');
+        $('html').addClass('llock');
+    });
+
+    $('.popup__button_exit_order').on('click', function(){
+        $('.popup_order').removeClass('popup_show');
+        $('.popup__input').removeClass('error');
+        $('.popup__input').val('');
+        $('html').removeClass('llock');
+    });
+
+    function popupOrderOrder() {
+      let nameOrder = $('#popup__input_name_order').val(),
+          telOrder = $('#popup__input_tel_order').val(),
+          email = $('#popup__input_email').val();
+
+      if (nameOrder.length !== 0 && telOrder.length == 16 && email.length !== 0){
+          popupClose();
+      }
+
+      else if (nameOrder.length == 0 && telOrder.length == 16 && email.length !== 0){
+          $('#popup__input_name_order').addClass('error');
+          return false;
+      }
+  
+      else if (nameOrder.length !== 0 && telOrder.length !== 16 && email.length !== 0){
+          $('#popup__input_tel_order').addClass('error').val('');
+          return false;
+      }
+
+      else if (nameOrder.length !== 0 && telOrder.length == 16 && email.length == 0){
+        $('#popup__input_email').addClass('error');
+        return false;
+      }
+
+      else if (nameOrder.length == 0 && telOrder.length !== 16 && email.length !== 0){
+          $('#popup__input_tel_order').addClass('error').val('');
+          $('#popup__input_name_order').addClass('error');
+          return false;
+      }
+
+      else if (nameOrder.length !== 0 && telOrder.length !== 16 && email.length == 0){
+        $('#popup__input_tel_order').addClass('error').val('');
+        $('#popup__input_email').addClass('error');
+        return false;
+      }
+
+      else if (nameOrder.length == 0 && telOrder.length == 16 && email.length == 0){
+          $('#popup__input_name_order').addClass('error');
+          $('#popup__input_email').addClass('error');
+          return false;
+      }
+  
+      else {
+          $('.popup__input').addClass('error');
+          setTimeout(function(){
+            $('.popup__input').removeClass('error');
+          }, 1500);
+          return false;
+      }
+
+      popupGraditudeOpen();
     }
 
-    popupOpenCall.forEach(btn => btn.addEventListener('click', showPopupCall));
-    popupCloseCall.addEventListener('click', closePopupCall);
-
-    // POPUP-ORDER
-
-    const popupOpenOrder = document.querySelectorAll('.popup_order_open');
-    const popupCloseOrder = document.querySelector('.popup__button_exit_order');
-    const popupOrder = document.querySelector('.popup_order');
-
-    function showPopupOrder() {
-      popupOrder.classList.add('popup_show');
-      hidden.classList.add('html_hidden');
-    }
-
-    function closePopupOrder() {
-      popupOrder.classList.remove('popup_show');
-      hidden.classList.remove('html_hidden');
-    }
-
-    popupOpenOrder.forEach(btn => btn.addEventListener('click', showPopupOrder));
-    popupCloseOrder.addEventListener('click', closePopupOrder);
+    $('.popup__input_send_order').on('click', popupOrderOrder);
 
     // TEL-MASK
 
